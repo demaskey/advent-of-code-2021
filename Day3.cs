50f10b49976f1eb4abcb80f1ae5ed5e2c81953e2
Christoph DeMaskey <christoph.demaskey@gmail.com>
2021-12-15 23:23:37 -0500

    solved day 3, puzzle 1

---
+++ Day3.cs
@@ -0,0 +1,97 @@
+using System.Text;
+
+public class Day3
+{
+    private string[] diagLines;
+
+    public Day3()
+    {
+        Console.WriteLine("Reading Diagnostic Binary lines from file day3-puzzl1");
+
+        diagLines = System.IO.File.ReadAllLines("day3-puzzle1");
+
+        Console.WriteLine("Read lines into string array");
+        Console.WriteLine("Line Count: {0}", diagLines.Length);
+    }
+
+    public void Puzzle1()
+    {
+        int[] countOfOnes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] countOfZeros = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        foreach(string line in diagLines)
+        {
+            // Console.WriteLine("Line of bits: {0}", line);
+            for(int count = 0; count < line.Length; count++)
+            {
+                char character = line[count];
+                // Console.WriteLine("Character: {0}", character);
+                
+                switch(character)
+                {
+                    case '0':
+                        countOfZeros[count] += 1;
+                        break;
+                    case '1':
+                        countOfOnes[count] += 1;
+                        break;
+                    default:
+                        Console.WriteLine("Something is wrong. We got to a default case in a switch statement that shouldn't have happened.");
+                        break;
+                }
+                
+            }
+        }
+
+        Console.WriteLine("Count of ones array: {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}", countOfOnes[0], countOfOnes[1], countOfOnes[2], countOfOnes[3], countOfOnes[4], countOfOnes[5], countOfOnes[6], countOfOnes[7], countOfOnes[8], countOfOnes[9], countOfOnes[10], countOfOnes[11]);
+        Console.WriteLine("Count of zeors array: {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}", countOfZeros[0], countOfZeros[1], countOfZeros[2], countOfZeros[3], countOfZeros[4], countOfZeros[5], countOfZeros[6], countOfZeros[7], countOfZeros[8], countOfZeros[9], countOfZeros[10], countOfZeros[11]);
+
+        StringBuilder gammaRateBinary = new StringBuilder(), epsilonRateBinary = new StringBuilder();
+
+        for(int count = 0; count < countOfOnes.Length; count++)
+        {
+            if(countOfOnes[count] > countOfZeros[count])
+            {
+                gammaRateBinary.Append("1");
+                epsilonRateBinary.Append("0");
+            }
+            else
+            {
+                gammaRateBinary.Append("0");
+                epsilonRateBinary.Append("1");
+            }
+        }
+
+        Console.WriteLine("Gamma Rate in Binary: {0}", gammaRateBinary.ToString());
+        Console.WriteLine("Epsilon Rate in Binary: {0}", epsilonRateBinary.ToString());
+
+        double gammaRate = ConvertStringOfBitsToInteger(gammaRateBinary.ToString());
+        double epsilonRate = ConvertStringOfBitsToInteger(epsilonRateBinary.ToString());
+
+        Console.WriteLine("Gamma Rate: {0}", gammaRate);
+        Console.WriteLine("Epsilon Rate: {0}", epsilonRate);
+
+        double powerConsumption = gammaRate * epsilonRate;
+
+        Console.WriteLine("Power Consumption: {0}", powerConsumption);
+    }
+
+    public double ConvertStringOfBitsToInteger(string bits)
+    {
+        double returnValue = 0;
+
+        for(int count = 0; count < bits.Length; count++)
+        {
+            if(bits[count] == '1')
+            {
+                double power = bits.Length - 1 - count;
+
+                double value = Math.Pow(2,power);
+
+                returnValue += value;
+            }
+        }
+
+        return returnValue;
+    }
+}
